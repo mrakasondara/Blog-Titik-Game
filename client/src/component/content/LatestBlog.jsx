3137bfd85596069f3cb6536f8db1e46b3787d16d
mrakasondara <rakasondara21@gmail.com>
2023-12-23 06:08:00 +0700

    restore domain server

--- client/src/component/content/LatestBlog.jsx
+++ client/src/component/content/LatestBlog.jsx
@@ -5,7 +5,7 @@ const LatestBlog = () => {
   const [posts,setPosts] = useState({})
   const [isLoading, setIsLoading] = useState(true)
   useEffect(()=>{
-    fetch('https://titikgameserver.vercel.app/post').then(response=>{
+    fetch('http://localhost:4000/post').then(response=>{
       response.json().then(posts=>{
         setPosts(posts)
         setIsLoading(false)
--- client/src/component/content/LoginPage.jsx
+++ client/src/component/content/LoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
  const {setUserInfo} = useContext(UserContext)
  async function login(ev){
     ev.preventDefault()
-    const response = await fetch('https://titikgameserver.vercel.app/login',{
+    const response = await fetch('http://localhost:4000/login',{
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
