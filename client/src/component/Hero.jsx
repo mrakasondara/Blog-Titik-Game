1f66e19d6721a1d7da56fb59cca9c6fb3018a4e5
Muhammad Raka Sondara <119824640+mrakasondara@users.noreply.github.com>
2023-12-23 08:34:09 +0700

    Update Hero.jsx

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
   const [navigate, setNavigate] = useState(false)
 
   useEffect(()=>{
-    fetch('https://server-titik-game.vercel.app/api/highlight').then(response=>{
-    response.json().then(post=>{
-      const index = Math.floor(Math.random()*post.length)
-      setHighlight(post[index])
-      setIsLoading(false)
+    fetch(`${import.meta.env.VITE_API_URL}/highlight`).then(response=>{
+      response.json().then(post=>{
+        const index = Math.floor(Math.random()*post.length)
+        setHighlight(post[index])
+        setIsLoading(false)
     })
   })
   },[])
@@ -22,7 +22,7 @@ const Hero = () => {
   }
   return (
     <div className="w-full flex flex-col items-center gap-5">
-      <div className='w-3/4 md:w-2/4 relative flex bg-[url(https://server-titik-game.vercel.app/api/uploads/hero.png)] bg-cover bg-center justify-center items-center h-20 rounded-lg'>
+      <div className=`w-3/4 md:w-2/4 relative flex bg-[url(${import.meta.env.VITE_API_URL}/uploads/hero.png)] bg-cover bg-center justify-center items-center h-20 rounded-lg`>
       <h1 className="text-2xl text-white font-fira">Blog</h1>
         <div className="flex absolute w-3/4 md:w-1/2 h-10 bg-indigo-500 top-[75%] rounded-lg items-center ">
         <input className="absolute w-[90%] h-10 bg-transparent rounded-lg text-white font-roboto px-3 outline-none" placeholder="Search"/>
