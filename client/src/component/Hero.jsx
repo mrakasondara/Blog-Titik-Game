4bb2fe20cd8e49029d069bdaafaca154d6cec8af
Muhammad Raka Sondara <119824640+mrakasondara@users.noreply.github.com>
2023-12-23 07:14:45 +0700

    Update Hero.jsx

--- client/src/component/Hero.jsx
+++ client/src/component/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   const [navigate, setNavigate] = useState(false)
 
   useEffect(()=>{
-    fetch('https://blog-titik-game.vercel.app/highlight').then(response=>{
+    fetch('https://blog-titik-game.vercel.app/api/highlight').then(response=>{
     response.json().then(post=>{
       const index = Math.floor(Math.random()*post.length)
       setHighlight(post[index])
@@ -22,7 +22,7 @@ const Hero = () => {
   }
   return (
     <div className="w-full flex flex-col items-center gap-5">
-      <div className='w-3/4 md:w-2/4 relative flex bg-[url(https://blog-titik-game.vercel.app/uploads/hero.png)] bg-cover bg-center justify-center items-center h-20 rounded-lg'>
+      <div className='w-3/4 md:w-2/4 relative flex bg-[url(https://blog-titik-game.vercel.app/api/uploads/hero.png)] bg-cover bg-center justify-center items-center h-20 rounded-lg'>
       <h1 className="text-2xl text-white font-fira">Blog</h1>
         <div className="flex absolute w-3/4 md:w-1/2 h-10 bg-indigo-500 top-[75%] rounded-lg items-center ">
         <input className="absolute w-[90%] h-10 bg-transparent rounded-lg text-white font-roboto px-3 outline-none" placeholder="Search"/>
@@ -40,7 +40,7 @@ const Hero = () => {
       )}
       {!isLoading&&(
         <div className="flex-col flex md:flex-row w-full my-5 items-center justify-center px-5">
-            <img src={`https://titikgameserver.vercel.app/${highlight?.thumbnail}`} onClick={()=> setNavigate(true)} alt="thumbnail" className="w-full justify-center md:w-2/5 rounded-lg cursor-pointer"/>
+            <img src={`https://blog-titik-game.vercel.app/api/${highlight?.thumbnail}`} onClick={()=> setNavigate(true)} alt="thumbnail" className="w-full justify-center md:w-2/5 rounded-lg cursor-pointer"/>
             <div className="md:w-1/2 flex flex-col justify-center items-center gap-2 p-2 mx-10">
             <Link to={`/detailpost/${highlight._id}`} className="text-l text-center font-roboto mt-2">{highlight?.title}</Link>
             <div className="flex justify-center md:justify-start gap-2">
