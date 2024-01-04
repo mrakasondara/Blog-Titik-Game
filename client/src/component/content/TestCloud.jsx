90a448726af8bf4e52b1d728e1776fce89b3e8f5
mrakasondara <rakasondara21@gmail.com>
2024-01-04 10:56:56 +0700

    tes cloudify

---
+++ client/src/component/content/TestCloud.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+const TestCloud =()=>{
+	const [files, setFiles] = useState('')
+	const create = (ev)=>{
+		const data = new FormData()
+		data.set('Files',files)
+		ev.preventDefault()
+		await fetch('https://api-tes-rho.vercel.app/post',{
+			method: 'POST',
+			body: data,
+			credentials: 'include'
+		})
+	}
+	return(
+		<div className="w-full flex justify-center items-center">
+			<input type="file" onChange={(ev)=> setFiles(ev.target.value)}/>			
+		</div>
+	)
+}
+export default TestCloud
