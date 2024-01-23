60b63be3d37f8248d9389b3f26b5004b3c30146f
mrakasondara <rakasondara21@gmail.com>
2024-01-23 18:14:23 +0700

    tes internal server

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 const multer = require('multer')
+const cloudinary = require('cloudinary').v2
 const fs = require('fs')
 const env = require('dotenv')
 const User = require('../model/User')
@@ -12,17 +13,31 @@ const Post = require('../model/Post')
 
 const app = express()
 const port = process.env.port || 4000
-
-const uploadMiddleware = multer({dest: 'uploads/'})
+  
+// const uploadMiddleware = multer({dest: 'uploads/'})
 
 const salt = bcrypt.genSaltSync(10)
 const secret = '1dhds9sdfs982snqwiqdh'
 // http://localhost:4000
 mongoose.connect('mongodb+srv://rakasondara21:rakasondara21@project.ezg1faq.mongodb.net/?retryWrites=true&w=majority')            
 app.use(express.json())
-app.use(cors({credentials:true,origin:'https://blog-titik-games.vercel.app'}))
+app.use(cors({credentials:true,origin:'https://blog-titik-game.vercel.app'}))
 app.use(cookieParser())
-app.use('/uploads', express.static(__dirname + '/uploads'));
+// app.use('/uploads', express.static(__dirname + '/uploads'));
+
+const storage = multer.diskStorage({
+    filename: function(req,file,cb){
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage:storage})
+
+cloudinary.config({
+    cloud_name:'dxs0jt3xe',
+    api_key: '531926252978129',
+    api_secret:'DuGN2Yq0sYUfzvCDnAQf9nLhIV4',
+})
 
 app.post('/register',async (req,res)=>{
     const {fullname,username,password} = req.body
@@ -62,11 +77,6 @@ app.post('/login',async (req,res)=>{
 
 })
 
-app.get('/hello', (req,res)=>{
-    res.send('Hello')
-})
-
-
 app.get('/profile', (req,res)=>{
     const {token} = req.cookies
     if(token){
@@ -85,11 +95,16 @@ app.post('/logout', (req,res)=>{
 const addBlog = (token,title,summary,tag,newPath,content)=>{
     jwt.verify(token,secret,{}, async (err,info)=>{
         if(err)throw err;
+         let newName
+            cloudinary.uploader.upload(path, {folder: 'uploads'}).then(result=>{
+                newName = result.public_id + '.' + result.format
+            })
+            mongoose.connect("mongodb+srv://rakasondara21:rakasondara21@project.ezg1faq.mongodb.net/?retryWrites=true&w=majority")
             postDoc = await Post.create({
                 title,
                 summary,
                 tag,
-                thumbnail:newPath,
+                thumbnail:newName,
                 content,
                 author:info.id,
             })
@@ -99,7 +114,7 @@ const addBlog = (token,title,summary,tag,newPath,content)=>{
 
 
 
-app.post('/createpost', uploadMiddleware.single('file'), async (req,res)=>{
+app.post('/createpost', upload.single('file'), async (req,res)=>{
     if(req.file === undefined){
         res.status(400).json('fill the thumbnail')
     }else{
@@ -107,25 +122,26 @@ app.post('/createpost', uploadMiddleware.single('file'), async (req,res)=>{
         const parts = originalname.split('.')
         const ext = parts[parts.length - 1]
         const lowerExt = ext.toLowerCase()
-        const newPath = path+'.'+ext
+        // const newPath = path+'.'+ext
         let postDoc
         const {token} = req.cookies
-        fs.renameSync(path, newPath)
+        const {title,summary,tag,content} = req.body
+        // fs.renameSync(path, newPath)
         switch(lowerExt){
             case 'jpg':
-            addBlog(token,title,summary,tag,newPath,content)
+            addBlog(token,title,summary,tag,path,content)
             res.status(200).json(postDoc)
             break;
             case 'jpeg':
-            addBlog(token,title,summary,tag,newPath,content)
+            addBlog(token,title,summary,tag,path,content)
             res.status(200).json(postDoc)
             break;
             case 'png':
-            addBlog(token,title,summary,tag,newPath,content)
+            addBlog(token,title,summary,tag,path,content)
             res.status(200).json(postDoc)
             break;
             case 'webp':
-            addBlog(token,title,summary,tag,newPath,content)
+            addBlog(token,title,summary,tag,path,content)
             res.status(200).json(postDoc)
             break;
             default:
@@ -153,43 +169,43 @@ const editBlog = (token,id,title,summary,tag,newPath,content, postDoc)=>{
     })    
 }
 
-app.put('/post',uploadMiddleware.single('file'),async(req,res)=>{
-    let newPath = null
-    const {token} = req.cookies
-    const {id,title,summary,tag,content} = req.body
-    const postDoc = await Post.findById(id)
-    if(req.file){
-        const {originalname,path} = req.file
-        const parts = originalname.split('.')
-        const ext = parts[parts.length - 1]
-        const lowerExt = ext.toLowerCase()
-        newPath = path+'.'+ext
-        fs.renameSync(path, newPath)
-        switch(lowerExt){
-            case 'jpg':
-            editBlog(token,id,title,summary,tag,newPath,content,postDoc)
-            res.status(200).json(postDoc)
-            break;
-            case 'jpeg':
-            editBlog(token,id,title,summary,tag,newPath,content,postDoc)
-            res.status(200).json(postDoc)
-            break;
-            case 'png':
-            editBlog(token,id,title,summary,tag,newPath,content,postDoc)
-            res.status(200).json(postDoc)
-            break;
-            case 'webp':
-            editBlog(token,id,title,summary,tag,newPath,content,postDoc)
-            res.status(200).json(postDoc)
-            break;
-            default:
-            res.status(400).json('image only')
-        }
-    }
-    editBlog(token,id,title,summary,tag,newPath,content,postDoc)
-    res.status(200).json(postDoc)
+// app.put('/post',uploadMiddleware.single('file'),async(req,res)=>{
+//     let newPath = null
+//     const {token} = req.cookies
+//     const {id,title,summary,tag,content} = req.body
+//     const postDoc = await Post.findById(id)
+//     if(req.file){
+//         const {originalname,path} = req.file
+//         const parts = originalname.split('.')
+//         const ext = parts[parts.length - 1]
+//         const lowerExt = ext.toLowerCase()
+//         newPath = path+'.'+ext
+//         fs.renameSync(path, newPath)
+//         switch(lowerExt){
+//             case 'jpg':
+//             editBlog(token,id,title,summary,tag,newPath,content,postDoc)
+//             res.status(200).json(postDoc)
+//             break;
+//             case 'jpeg':
+//             editBlog(token,id,title,summary,tag,newPath,content,postDoc)
+//             res.status(200).json(postDoc)
+//             break;
+//             case 'png':
+//             editBlog(token,id,title,summary,tag,newPath,content,postDoc)
+//             res.status(200).json(postDoc)
+//             break;
+//             case 'webp':
+//             editBlog(token,id,title,summary,tag,newPath,content,postDoc)
+//             res.status(200).json(postDoc)
+//             break;
+//             default:
+//             res.status(400).json('image only')
+//         }
+//     }
+//     editBlog(token,id,title,summary,tag,newPath,content,postDoc)
+//     res.status(200).json(postDoc)
     
-})
+// })
 
 app.get('/post', async (req,res)=>{
     res.json(
@@ -220,5 +236,9 @@ app.listen(port, ()=>{
     console.log(`App Listening on Port ${port}`)
 })
 
+
+// cloudinary.uploader.upload(path, {folder: 'uploads'}).then(result=>{
+ //                newName = result.public_id + '.' + result.format
+ //    })
 
 module.exports = app
